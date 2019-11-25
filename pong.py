62857309a7661c06dcd896b5315d91102e069843
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-11-25 01:55:50 -0500

    paddle two can move up and down

--- pong.py
+++ pong.py
@@ -48,10 +48,22 @@ def Paddle_1_down():
     y -= 20
     Paddle_1.sety(y)
 
+def Paddle_2_up():
+    y = Paddle_2.ycor()
+    y += 20
+    Paddle_2.sety(y)
+
+def Paddle_2_down():
+    y = Paddle_2.ycor()
+    y -= 20
+    Paddle_2.sety(y)
+
 # Keyboard binding
 wind.listen()
 wind.onkeypress(Paddle_1_up, "w")
 wind.onkeypress(Paddle_1_down, "s")
+wind.onkeypress(Paddle_2_up, "Up")
+wind.onkeypress(Paddle_2_down, "Down")
 
 # Main game loop
 while True:
