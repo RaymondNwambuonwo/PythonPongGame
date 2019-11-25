a25f0f9d23000a9df74996b8679694c9a7e10efb
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-11-25 02:02:53 -0500

    ball now bounces of top of screen

--- pong.py
+++ pong.py
@@ -74,3 +74,7 @@ while True:
     #  Move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
